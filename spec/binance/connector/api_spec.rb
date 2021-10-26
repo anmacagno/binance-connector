23477a2da5722b64bbbac7c7030fa579533febb0
anmacagno <anmacagno@gmail.com>
2021-10-26 13:11:29 -0500

    Adds unit tests

---
+++ spec/binance/connector/api_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+RSpec.describe Binance::Connector::Api do
+  describe '.url' do
+    it 'succeeds' do
+      expect(described_class.url('/path')).to eq(
+        "#{Binance::Connector::Api::BASE_URL}/path"
+      )
+    end
+  end
+
+  describe '.ping' do
+    let(:json) { JSON.parse(File.read('spec/fixtures/ping.json'), symbolize_names: true) }
+
+    before do
+      allow(Binance::Connector::HttpClient).to receive(:get).and_return(json)
+    end
+
+    it 'succeeds' do
+      expect(described_class.ping).to eq(
+        {}
+      )
+    end
+  end
+
+  describe '.time' do
+    let(:json) { JSON.parse(File.read('spec/fixtures/time.json'), symbolize_names: true) }
+
+    before do
+      allow(Binance::Connector::HttpClient).to receive(:get).and_return(json)
+    end
+
+    it 'succeeds' do
+      expect(described_class.time.keys).to match_array(
+        %i[serverTime]
+      )
+    end
+  end
+
+  describe '.exchange_info' do
+    before do
+      allow(Binance::Connector::HttpClient).to receive(:get).and_return(json)
+    end
+
+    context 'when the symbols are valid' do
+      let(:json) { JSON.parse(File.read('spec/fixtures/exchangeInfo.json'), symbolize_names: true) }
+
+      it 'succeeds' do
+        expect(described_class.exchange_info(%w[BTCUSDT ETHUSDT]).keys).to match_array(
+          %i[timezone serverTime rateLimits exchangeFilters symbols]
+        )
+      end
+    end
+  end
+
+  describe '.avg_price' do
+    before do
+      allow(Binance::Connector::HttpClient).to receive(:get).and_return(json)
+    end
+
+    context 'when the symbol is valid' do
+      let(:json) { JSON.parse(File.read('spec/fixtures/avgPrice.json'), symbolize_names: true) }
+
+      it 'succeeds' do
+        expect(described_class.avg_price('ETHUSDT').keys).to match_array(
+          %i[mins price]
+        )
+      end
+    end
+  end
+end
