c954dcef9d924418799093e22558f91f9a17b8c7
anmacagno <anmacagno@gmail.com>
2023-02-15 13:53:47 -0600

    Updates gems and bundler. Adds require_relative for contracts.

--- lib/binance/connector.rb
+++ lib/binance/connector.rb
@@ -4,7 +4,16 @@ require_relative 'connector/api'
 require_relative 'connector/http_client'
 require_relative 'connector/version'
 require_relative 'connector/contracts/application_contract'
-Dir['./lib/binance/connector/contracts/*.rb'].each { |f| require f }
+require_relative 'connector/contracts/avg_price_contract'
+require_relative 'connector/contracts/cancel_order_contract'
+require_relative 'connector/contracts/exchange_info_contract'
+require_relative 'connector/contracts/get_open_orders_contract'
+require_relative 'connector/contracts/get_order_contract'
+require_relative 'connector/contracts/get_orders_contract'
+require_relative 'connector/contracts/klines_contract'
+require_relative 'connector/contracts/my_trades_contract'
+require_relative 'connector/contracts/new_order_contract'
+require_relative 'connector/contracts/ticker_price_contract'
 
 module Binance
   module Connector
